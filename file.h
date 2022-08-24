94ac645f6133f3ec86011a726e0c8fe15056ebaa
Rushana Denisheva <denisheva.rr@phystech.edu>
2022-08-24 13:19:45 +0300

    documentation

--- file.h
+++ file.h
@@ -1,4 +1,6 @@
 #ifndef FILE_H_
+
+//! Check if this file wasn't included more than 1 time
 #define FILE_H_
 
 #include <stdio.h>
@@ -38,9 +40,9 @@ enum roots{
 //----------------------------------------------------------------
 //! Announce coefficients in the square equation ax^2 + bx + c = 0
 //!
-//! @param [in] Pointer to the a-coefficient
-//! @param [in] Pointer to the b-coefficient
-//! @param [in] Pointer to the c-coefficient
+//! @param [in] a Pointer to the a-coefficient
+//! @param [in] b Pointer to the b-coefficient
+//! @param [in] c Pointer to the c-coefficient
 //----------------------------------------------------------------
 
 void AnnounceCoeff(double* a, double* b, double* c);
@@ -48,7 +50,9 @@ void AnnounceCoeff(double* a, double* b, double* c);
 //-------------------------------------------
 //! Check if the entered symbol the number is
 //! 
-//! @return Non-numeric symbols
+//! @param [out] ch Non-numeric symbols
+//!
+//! @return Number that was entered
 //-------------------------------------------
 
 double GetDouble(void);
@@ -56,9 +60,9 @@ double GetDouble(void);
 //-----------------------------------------------
 //! Compare 2 double numbers with help of epsylon
 //!
-//! @param [in] First number
-//! @param [in] Second number
-//! @param [in] epsylon 
+//! @param [in] x First number
+//! @param [in] y Second number
+//! @param [in] epsylon Constant for comparing
 //! 
 //! @return 1 if two double numbers are equal(0 if not)
 //------------------------------------------------
@@ -68,11 +72,11 @@ int CompareNumbers(double  x, double  y, const double  epsylon);
 //--------------------------------------------------------
 //! Solve the square equation ax^2 + bx + c = 0
 //!
-//! @param [in] a-coefficient
-//! @param [in] b-coefficient
-//! @param [in] c-coefficient
-//! @param [out] Pointer to the 1st root
-//! @param [out] Pointer to the 2nd root
+//! @param [in] a a-coefficient
+//! @param [in] b b-coefficient
+//! @param [in] c c-coefficient
+//! @param [out] x_1 Pointer to the 1st root
+//! @param [out] x_2 Pointer to the 2nd root
 //!
 //! @return Number of roots
 //! 
@@ -84,9 +88,9 @@ int SolveSquare(double a, double b, double c, double* x_1, double* x_2);
 //--------------------------------
 //! Solve the equation bx + c = 0
 //!
-//! @param [in] b-coefficient
-//! @param [in] c-coefficient
-//! @param [out] Pointer to the root
+//! @param [in] b b-coefficient
+//! @param [in] c c-coefficient
+//! @param [out] x_1 Pointer to the root
 //!
 //! @return Number of roots
 //--------------------------------
@@ -96,13 +100,13 @@ int SolveLinear(double b, double c, double* x_1);
 //---------------------------------------------------------------------------------------
 //! Announce coefficients, number of roots and roots for debug from file "unit_tests.txt"
 //!
-//! @param [in] Pointer to file "unit_tests.txt"
-//! @param [in] Pointer to the a-coefficient from file
-//! @param [in] Pointer to the b-coefficient from file
-//! @param [in] Pointer to the c-coefficient from file
-//! @param [in] Pointer to the number of roots from file
-//! @param [in] Pointer to the first root from file
-//! @param [in] Pointer to the second root from file
+//! @param [in] fp Pointer to file "unit_tests.txt"
+//! @param [in] a Pointer to the a-coefficient from file
+//! @param [in] b Pointer to the b-coefficient from file
+//! @param [in] c Pointer to the c-coefficient from file
+//! @param [in] test_root_quantity Pointer to the number of roots from file
+//! @param [in] test_x_1 Pointer to the first root from file
+//! @param [in] test_x_2 Pointer to the second root from file
 //----------------------------------------------------------------------------------------
 
 void DebugScan(FILE* fp, double* a, double* b, double* c, int* test_root_quantity, double* test_x_1, double* test_x_2);
@@ -110,13 +114,13 @@ void DebugScan(FILE* fp, double* a, double* b, double* c, int* test_root_quantit
 //----------------------------------------------------------------------------------
 //! Solve the square equation ax^2 + bx + c = 0 with the coefficients from file and check the resulting roots with our function <int solve> with test roots
 //!
-//! @param [in] Pointer to file "unit_tests.txt"
-//! @param [in] a-coefficient
-//! @param [in] b-coefficient
-//! @param [in] c-coefficient
-//! @param [in] quantity of roots
-//! @param [in] 1st root
-//! @param [in] 2nd root
+//! @param [in] fp Pointer to file "unit_tests.txt"
+//! @param [in] a a-coefficient
+//! @param [in] b b-coefficient
+//! @param [in] c c-coefficient
+//! @param [in] root_quantity quantity of roots
+//! @param [in] x_1 1st root
+//! @param [in] x_2 2nd root
 //! 
 //! @note The function compares quantity of roots and roots from file with the results of our program
 //-----------------------------------------------------------------------------------
@@ -126,14 +130,10 @@ void DebugSolveSquare(FILE* fp, double a, double b, double c, int root_quantity,
 //----------------------------------------------
 //! Switch for printing the roots
 //!
-//! @param [in] Pointer to file "unit_tests.txt"
-//! @param [in] a-coefficient
-//! @param [in] b-coefficient
-//! @param [in] c-coefficient
-//! @param [in] quantity of roots
-//! @param [in] 1st root
-//! @param [in] 2nd root
+//! @param [in] root_quantity quantity of roots
+//! @param [in] x_1 1st root
+//! @param [in] x_2 2nd root
 //-----------------------------------------------
 
-void PrintRoots(int n, double x_1, double x_2);
+void PrintRoots(int root_quantity, double x_1, double x_2);
 #endif
